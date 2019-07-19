6c1b2153ec283d28e48091a5fe2dfd1cf08c5e27
ruichuan.li <ruichuan.li@jimu.com>
2019-07-19 14:46:13 +0800

    增加获取免费代理ip的代码

--- demo/pipelines.py
+++ demo/pipelines.py
@@ -2,6 +2,7 @@
 import os
 import json
 import codecs
+import pymysql
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -21,4 +22,39 @@ class DemoPipeline(object):
         with codecs.open(filename, 'a') as f:
             line = json.dumps(dict(item), ensure_ascii=False) + '\n'
             f.write(line)
+        return item
+
+#写入数据到mysql 如果需要可以配置到settings ITEM_PIPELINES 中来执行
+class W2Mysql(object):
+    def process_item(self,item,spider):
+        # 日期
+        day = item['day']
+        # 天气
+        wea = item['wea']
+        # 温度
+        tem = item['tem']
+        # 风力
+        wind = item['wind']
+
+        # 和本地的scrapyDB数据库建立连接
+        connection = pymysql.connect(
+            host='localhost',  # 连接的是本地数据库
+            user='root',  # 自己的mysql用户名
+            passwd='123456',  # 自己的密码
+            db='scrapyDB',  # 数据库的名字
+            charset='utf8mb4',  # 默认的编码方式：
+            cursorclass=pymysql.cursors.DictCursor)
+        try:
+            with connection.cursor() as cursor:
+                # 创建更新值的sql语句
+                sql = """INSERT INTO WEATHER(day,wea,tem,wind)
+                                VALUES (%s, %s,%s,%s,%s,%s)"""
+                # 执行sql语句
+                # excute 的第二个参数可以将sql缺省语句补全，一般以元组的格式
+                cursor.execute(sql, (day, wea, tem, wind))
+                # 提交
+            connection.commit()
+        finally:
+            #关闭连接
+            connection.close()
         return item
---
+++ demo/spiders/proxy.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import threading
+import telnetlib
+
+#初始换浏览器header
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0"}
+
+def get_ip():
+    #获取代理ip 返回列表数据
+    httpResult=[]
+    httpsResult=[]
+    IPBaseUrl = "https://www.kuaidaili.com/ops/proxylist/"
+    print("-------in------")
+    try:
+        for page in range(1,10):
+            IPPageUrl = IPBaseUrl + str(page)
+            IPResp = requests.get(IPPageUrl,headers=headers)
+            IPText = IPResp.text
+            soupIP = BeautifulSoup(IPText,"lxml")
+            freeListDiv = soupIP.find('div',id='freelist')
+            IPTrs = freeListDiv.find_all('tr')
+            for IPTr in IPTrs[1:]:
+                tds = IPTr.find_all('td')
+                ip = tds[0].text.strip()
+                port = tds[1].text.strip()
+                protocols = tds[3].text.strip().split(',')
+                for p in protocols:
+                    if p.strip() == 'HTTP':
+                        httpResult.append('http://' + ip + ':' + port)
+                    elif p.strip() == 'HTTPS':
+                        httpsResult.append('https://' + ip + ':' + port)
+                    else:
+                        print("非法的协议：",p)
+
+    except BaseException as e:
+        print("exception:",e)
+    return httpResult,httpsResult
+#验证ip地址的可用性 可用telnetlib模块进行校验
+def check_http(ip,pt):
+    f = open("D:\work-self\scrapy-demo\demo\demo\data\ip_http.txt","a")
+    f.truncate()
+    try:
+        telnetlib.Telnet(ip,port=pt,timeout=5)
+    except:
+        print("fail:",ip+":"+pt)
+    else:
+        print("success:", ip + ":" + pt)
+        f.write(ip+":"+pt+"\n")
+
+# 验证ip地址的可用性 可用telnetlib模块进行校验
+def check_https(ip,pt):
+    f = open("D:\work-self\scrapy-demo\demo\demo\data\ip_https.txt","a")
+    f.truncate()
+    try:
+        telnetlib.Telnet(ip,port=pt,timeout=5)
+    except:
+        print("fail:",ip+":"+pt)
+    else:
+        f.write(ip+":"+pt+"\n")
+
+#使用代理访问网站 来验证是否可用
+def check_by_net(pro,ip,pt):
+    url=''
+    if pro.strip() == 'HTTP':
+        url=ip + ':' + pt
+    elif pro.strip() == 'HTTPS':
+        url = ip + ':' + pt
+    else:
+        print("非法的协议：", pro)
+    try:
+        print("proxy url:",url)
+        requests.get("https://www.autohome.com.cn/beijing/",headers=headers,proxies={'http':url},timeout=3)
+    except BaseException as e:
+        print("出现异常：",e)
+    else:
+        print('---------------------------success:',url)
+
+def main():
+    httpResult,httpsResult=get_ip()
+    print("http size is :",len(httpResult))
+    print("https size is :",len(httpsResult))
+    if len(httpResult) == 0 and len(httpsResult) == 0:
+        print("没有可执行数据")
+        pass
+
+    #循环执行http数据 telnet判断有效性
+    if len(httpResult) > 0:
+        threads = []
+        open("D:\work-self\scrapy-demo\demo\demo\data\ip_http.txt", "a").truncate()
+        for i in httpResult:
+            ip = str(i.split(":")[-2][2:].strip())
+            pt = str(i.split(":")[-1].strip())
+            #t = threading.Thread(target=check_by_net, args=('HTTP',ip, pt))
+            t = threading.Thread(target=check_http, args=(ip, pt))
+            threads.append(t)
+        for i in range(len(httpResult)):
+            threads[i].start()
+
+        for i in range(len(httpResult)):
+            threads[i].join()
+
+    #循环https 数据
+    if len(httpsResult) > 0:
+        threads_https = []
+        open("D:\work-self\scrapy-demo\demo\demo\data\ip_https.txt", "a").truncate()
+        for i in httpsResult:
+            ip = str(i.split(":")[-2][2:].strip())
+            pt = str(i.split(":")[-1].strip())
+            t = threading.Thread(target=check_https, args=(ip, pt))
+            threads_https.append(t)
+
+        for i in range(len(httpsResult)):
+            threads_https[i].start()
+
+        for i in range(len(httpsResult)):
+            threads_https[i].join()
+
+
+if __name__ == '__main__':
+    main()
